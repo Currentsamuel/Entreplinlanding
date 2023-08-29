bc3ec1cd87a2d70769f4bdb1fe7e08061eb4958c
Daniekeys-official <68125935+Daniekeys@users.noreply.github.com>
2023-08-29 02:33:26 +0100

    Update Hero.tsx

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className="md:w-6/12 flex flex-col lg:mt-20">
           <h1 className="sat-bold text-[30px] tracking-[0.75px] md:text-[50px] text-primary font-bold">
           Entrepreneurs' Platform to Buy, Sell{" "}
-            <span className="text-buttonColor">Bussinesses</span>, Partner and Discuss.
+            <span className="text-buttonColor">Businesses</span>, Partner and Discuss.
           </h1>
           <p className="text-[#6D6D6D] text-base sat-regular mt-5 md:mt-[10px]">
           Connecting entrepreneurs all over the places to buy and sell businesses, partner, give and receive support
