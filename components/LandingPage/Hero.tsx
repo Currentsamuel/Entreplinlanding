6e96a335b2964a38f1a8a3ef0b74a3c1649dc948
akindunjoyetolulope <akindunjoyetolulope5@gmail.com>
2024-03-27 20:01:20 +0100

    links added

--- components/LandingPage/Hero.tsx
+++ components/LandingPage/Hero.tsx
@@ -1,34 +1,62 @@
-import React from 'react'
-import ContainerLayout from '../../Layouts/ContainerLayout'
-import Image from 'next/image'
-import google from "../../assets/svg/google-logo.svg"
-import apple from "../../assets/svg/apple-logo.svg"
-import hero from "../../assets/svg/hero.svg"
+import React, { useEffect } from "react";
+import ContainerLayout from "../../Layouts/ContainerLayout";
+import Image from "next/image";
+import google from "../../assets/svg/google-logo.svg";
+import apple from "../../assets/svg/apple-logo.svg";
+import hero from "../../assets/svg/hero.svg";
+import { getMobileOperatingSystem } from "../../utils/helper";
+
 const Hero = () => {
+  const [mobileOperating, setMobileOperating] = React.useState<string>();
+
+  useEffect(() => {
+    setMobileOperating(getMobileOperatingSystem());
+  }, []);
+
+  const onNavigate = () => {
+    if (mobileOperating === "iOS") {
+      window.location.href =
+        "https://apps.apple.com/id/app/entreplin/id6463440642";
+    } else if (mobileOperating === "Android") {
+      window.location.href =
+        "https://play.google.com/store/apps/details?id=com.entreplin";
+    }
+  };
+
   return (
     <ContainerLayout>
       <div className="w-full flex md:flex-row flex-col md:justify-between mt-32 md:mt-[120px] space-x-6 ">
         <div className="md:w-6/12 flex flex-col lg:mt-20">
           <h1 className="sat-bold text-[30px] tracking-[0.75px] md:text-[50px] text-primary font-bold">
-          Entrepreneurs' Platform to Buy, Sell{" "}
-            <span className="text-buttonColor">Businesses</span>, Partner and Discuss.
+            Entrepreneurs' Platform to Buy, Sell{" "}
+            <span className="text-buttonColor">Businesses</span>, Partner and
+            Discuss.
           </h1>
           <p className="text-[#6D6D6D] text-base sat-regular mt-5 md:mt-[10px]">
-          Connecting entrepreneurs all over the places to buy and sell businesses, partner, give and receive support
+            Connecting entrepreneurs all over the places to buy and sell
+            businesses, partner, give and receive support
           </p>
           <div className="flex items-center space-x-4  mt-[22px]">
-            <Image
-              src={apple}
-              width={apple.width}
-              height={apple.height}
-              alt="apple"
-            />
-            <Image
-              src={google}
-              width={google.width}
-              height={google.height}
-              alt="google"
-            />
+            {mobileOperating === "iOS" && (
+              <button onClick={onNavigate}>
+                <Image
+                  src={apple}
+                  width={apple.width}
+                  height={apple.height}
+                  alt="apple"
+                />
+              </button>
+            )}
+            {mobileOperating === "Android" && (
+              <button onClick={onNavigate}>
+                <Image
+                  src={google}
+                  width={google.width}
+                  height={google.height}
+                  alt="google"
+                />
+              </button>
+            )}
           </div>
         </div>
         <div className="w-full md:w-6/12 mt-8 md:mt-0">
@@ -37,6 +65,6 @@ const Hero = () => {
       </div>
     </ContainerLayout>
   );
-}
+};
 
-export default Hero
+export default Hero;
---
+++ utils/helper.ts
@@ -0,0 +1,12 @@
+export function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor;
+
+  if (/android/i.test(userAgent)) {
+    return "Android";
+  }
+  if (/iPad|iPhone|iPod/.test(userAgent)) {
+    return "iOS";
+  }
+
+  return "unknown";
+}
